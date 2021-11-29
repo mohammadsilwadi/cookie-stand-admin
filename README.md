a70a27347c374363f844fd83f0638bfacd748639
mohammad silwadi <mohammadsilwadi1@gmail.com>
2021-11-29 19:21:06 +0200

    lab37

--- README.md
+++ README.md
@@ -1,29 +1,15 @@
-# Next.js + Tailwind CSS Example
+# lab 37
+## Feature Tasks and Requirements
+The spec for lab is screen shot of Cookie Stand Admin Version 1
 
-This example shows how to use [Tailwind CSS](https://tailwindcss.com/) [(v2.2)](https://blog.tailwindcss.com/tailwindcss-2-2) with Next.js. It follows the steps outlined in the official [Tailwind docs](https://tailwindcss.com/docs/guides/nextjs).
+pages/Index.js should…
 
-It uses the new [`Just-in-Time Mode`](https://tailwindcss.com/docs/just-in-time-mode) for Tailwind CSS.
+Have ```<Head>``` component with page title set to Cookie Stand Admin
 
-## Preview
+Have a header component that matches spec.
 
-Preview the example live on [StackBlitz](http://stackblitz.com/):
+Have a ```<main>``` component containing ```<form>``` and a placeholder component showing JSON string of last created Cookie Stand.
 
-[![Open in StackBlitz](https://developer.stackblitz.com/img/open_in_stackblitz.svg)](https://stackblitz.com/github/vercel/next.js/tree/canary/examples/with-tailwindcss)
+Have a ```<footer>``` component that matches spec.
 
-## Deploy your own
-
-Deploy the example using [Vercel](https://vercel.com?utm_source=github&utm_medium=readme&utm_campaign=next-example):
-
-[![Deploy with Vercel](https://vercel.com/button)](https://vercel.com/new/git/external?repository-url=https://github.com/vercel/next.js/tree/canary/examples/with-tailwindcss&project-name=with-tailwindcss&repository-name=with-tailwindcss)
-
-## How to use
-
-Execute [`create-next-app`](https://github.com/vercel/next.js/tree/canary/packages/create-next-app) with [npm](https://docs.npmjs.com/cli/init) or [Yarn](https://yarnpkg.com/lang/en/docs/cli/create/) to bootstrap the example:
-
-```bash
-npx create-next-app --example with-tailwindcss with-tailwindcss-app
-# or
-yarn create next-app --example with-tailwindcss with-tailwindcss-app
-```
-
-Deploy it to the cloud with [Vercel](https://vercel.com/new?utm_source=github&utm_medium=readme&utm_campaign=next-example) ([Documentation](https://nextjs.org/docs/deployment)).
+[pr]()
--- pages/index.js
+++ pages/index.js
@@ -1,82 +1,81 @@
 import Head from 'next/head'
+import { useState } from 'react'
 
 export default function Home() {
+
+  const [cookieStand, set_data] = useState([]);
+  function Handler(event) {
+    event.preventDefault();
+
+    const save_data = {
+      location: event.target.location.value,
+      min_customers: event.target.minimum.value,
+      max_customers: event.target.Maximum.value,
+      avg_cookies: event.target.avgCustomers.value,
+
+    }
+    set_data(cookieStand => [...cookieStand, save_data])
+
+  }
   return (
-    <div className="flex flex-col items-center justify-center min-h-screen py-2">
+    <div className="">
       <Head>
         <title>Create Next App</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
+      <header className="py-5 bg-green-700" >
+        <h1 className="text-2xl">Cookie Stand Admin</h1>
+      </header>
+      <main className="items-center justify-center px-20 text-center ">
+        <form onSubmit={Handler} className="m-8 p-4 bg-green-300 rounded-md" >
+          <h2 className="text-center text-2xl">Cookie Stand Admin  </h2>
+          <div className="w-100">
+            <label className=" w-1/3"> Location </label>
+            <input className="bg-gray-100 m-3 w-2/3" name="location" type="text" />
+          </div>
+          <div className="flex items-stretch">
+            <div className="m-3 p-4 ">
+              <label className=" m-3 "> Minimum Customer pre Hour  </label>
+              <input className="bg-gray-100 m-3 w-2/3" name="minimum" type="number" />
+            </div>
 
-      <main className="flex flex-col items-center justify-center w-full flex-1 px-20 text-center">
-        <h1 className="text-6xl font-bold">
-          Welcome to{' '}
-          <a className="text-blue-600" href="https://nextjs.org">
-            Next.js!
-          </a>
-        </h1>
+            <div className="m-3 p-4 ">
+              <label className=" m-3 "> Maximum Customer pre Hour </label>
+              <input className="bg-gray-100 m-3 w-2/3" name="Maximum" type="number" />
+            </div>
+            <div className="m-3 p-4 ">
+              <label className=" m-3 "> Average Cookies pre sale  </label>
+              <input className="bg-gray-100 m-3 w-2/3" name="avgCustomers" type="number" />
+            </div>
 
-        <p className="mt-3 text-2xl">
-          Get started by editing{' '}
-          <code className="p-3 font-mono text-lg bg-gray-100 rounded-md">
-            pages/index.js
-          </code>
-        </p>
+            <button class="bg-green-500 hover:bg-green-400 text-white font-bold h-12 px-4 border-b-4 border-green-700 hover:border-green-500 rounded">
+              Create
+            </button>
+          </div>
+        </form>
+        <h2 className='text-2xl'>
+          Report Table Coming Soon...
+        </h2>
 
-        <div className="flex flex-wrap items-center justify-around max-w-4xl mt-6 sm:w-full">
-          <a
-            href="https://nextjs.org/docs"
-            className="p-6 mt-6 text-left border w-96 rounded-xl hover:text-blue-600 focus:text-blue-600"
-          >
-            <h3 className="text-2xl font-bold">Documentation &rarr;</h3>
-            <p className="mt-4 text-xl">
-              Find in-depth information about Next.js features and API.
-            </p>
-          </a>
-
-          <a
-            href="https://nextjs.org/learn"
-            className="p-6 mt-6 text-left border w-96 rounded-xl hover:text-blue-600 focus:text-blue-600"
-          >
-            <h3 className="text-2xl font-bold">Learn &rarr;</h3>
-            <p className="mt-4 text-xl">
-              Learn about Next.js in an interactive course with quizzes!
-            </p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/master/examples"
-            className="p-6 mt-6 text-left border w-96 rounded-xl hover:text-blue-600 focus:text-blue-600"
-          >
-            <h3 className="text-2xl font-bold">Examples &rarr;</h3>
-            <p className="mt-4 text-xl">
-              Discover and deploy boilerplate example Next.js projects.
-            </p>
-          </a>
-
-          <a
-            href="https://vercel.com/import?filter=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className="p-6 mt-6 text-left border w-96 rounded-xl hover:text-blue-600 focus:text-blue-600"
-          >
-            <h3 className="text-2xl font-bold">Deploy &rarr;</h3>
-            <p className="mt-4 text-xl">
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
+        <div className="">
+          {
+            cookieStand.map(item => {
+              return (
+                <p class="">{JSON.stringify(item)}</p>
+              )
+            })
+          }
         </div>
       </main>
 
-      <footer className="flex items-center justify-center w-full h-24 border-t">
-        <a
-          className="flex items-center justify-center"
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <img src="/vercel.svg" alt="Vercel Logo" className="h-4 ml-2" />
-        </a>
+      <footer className="">
+        <p className="bg-green-700" >@2021</p>
       </footer>
     </div>
   )
 }
+
+
+
+
+
